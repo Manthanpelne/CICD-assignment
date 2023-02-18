afbe37c94829f2e928137b4515ccff7f09b8ce2e
Manthanpelne <111408525+Manthanpelne@users.noreply.github.com>
2023-02-18 20:10:34 +0530

    added eslint again

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express")
 const app = express()
 
+app.get("/",(req,res)=>{
+    value=>10
+    res.send("success!")
+})
+
 app.listen(4500,()=>{
     console.log("running at server 4500")
 })
